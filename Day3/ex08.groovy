6039e23a983a39224acf95d688c97aecf9bf1e17
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-10-10 15:11:09 +0100

    Modified Day 3 exercise 08 to address second part of question

--- Day3/ex08.groovy
+++ Day3/ex08.groovy
@@ -1,11 +1,23 @@
 /*
 Write a program that reads some text from the user and then writes the same text on screen, but each letter on a
 different line.
+Now modify your program to write each word (as defined by spaces) rather than letter on a different line.
 */
 
 print "Enter some text: "
 String s = System.console().readLine()
 
+boolean lastWasSpace = false
+
 for (int i = 0; i < s.size(); i++) {
-	println s.charAt(i)
+	char c = s.charAt(i)
+	if (c == ' ') {
+		if (!lastWasSpace) {
+			println ""
+		}
+		lastWasSpace = true
+	} else {
+		lastWasSpace = false
+		print c
+	}
 }
