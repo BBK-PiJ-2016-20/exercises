ca9b3d12f920686e24d61772427a5a6c71d309db
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-10-10 15:21:15 +0100

    Modify Day 3 exercise 09 to address second part of exercise

--- Day3/ex09.groovy
+++ Day3/ex09.groovy
@@ -1,16 +1,32 @@
 /*
 Write a program that reads some text from the user and then says how many letters ’e’ are there in that text.
+Then modify the program so that it reads the text from the user and then asks for a letter. The program should
+then say how many times you can find the letter in the text.
 */
 
 print "Enter some text: "
 String s = System.console().readLine()
 
-int es = 0
+char toCount
+boolean charEntered = false
 
-for (int i = 0; i < s.size(); i++) {
-	if (s.charAt(i) == 'e') {
-		es++
+while (!charEntered) {
+	print "Enter a character to count: "
+	String c = System.console().readLine()
+	if (c.length() != 1) {
+		println "Enter exactly one character"
+	} else {
+		toCount = c.charAt(0)
+		charEntered = true
 	}
 }
 
-println "\"" + s + "\" contains " + es + " letter 'e's"
+int counted = 0
+
+for (int i = 0; i < s.size(); i++) {
+	if (s.charAt(i) == toCount) {
+		counted++
+	}
+}
+
+println "\"" + s + "\" contains " + counted + " letter '" + toCount + "'s"
