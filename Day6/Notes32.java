a73b40e906d157e6586b92bcb7589d67b35ff695
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-10-19 11:51:24 +0100

    Add Day 6 exercise from notes

---
+++ Day6/Notes32.java
@@ -0,0 +1,51 @@
+class Notes32 {
+    private static class Results {
+        private int distinctions;
+        private int passes;
+        private int fails;
+        private int invalid;
+
+        private Results() {
+            distinctions = 0;
+            passes = 0;
+            fails = 0;
+            invalid = 0;
+        }        
+
+        public void print() {
+            System.out.println("Distinctions: " + distinctions);
+            System.out.println("Passes: " + passes);
+            System.out.println("Fails: " + fails);
+            System.out.println("And " + invalid + " invalid entries.");
+        }
+
+        public static Results read() {
+            Results results = new Results();
+            boolean finished = false;
+            do {
+                System.out.print("Enter a mark (-1 to end): ");
+                int mark = Integer.parseInt(System.console().readLine());
+                if (mark == -1) {
+                    finished = true;
+                } else if (mark < 0 || mark > 100 ) {
+                    results.invalid++;
+                } else if (mark >= 70) {
+                    results.distinctions++;
+                } else if (mark >= 50) {
+                    results.passes++;
+                } else {
+                    results.fails++;
+                }
+            } while (!finished);
+
+            return results;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Results results = Results.read();
+        results.print();
+    }
+}
