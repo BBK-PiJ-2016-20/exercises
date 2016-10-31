960561b5a0453c8a587723391be2775c68fabbdc
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-10-31 13:51:35 +0000

    Add solution to Day 9 exercise 1

---
+++ Day9/ComparatorTest.java
@@ -0,0 +1,7 @@
+public class ComparatorTest {
+    public static void main(String[] args) {
+        System.out.println(Comparator.getMax(1.0, 2.0));
+        System.out.println(Comparator.getMax(1, 2));
+        System.out.println(Comparator.getMax("1", "2"));
+    }
+}
