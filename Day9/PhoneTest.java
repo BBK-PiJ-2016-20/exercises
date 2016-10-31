3242c40f647c85b6867c4b532070edae51eb7864
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-10-31 14:01:28 +0000

    Add changes for Day 9 exercise 2.4

--- Day9/PhoneTest.java
+++ Day9/PhoneTest.java
@@ -1,19 +1,19 @@
 public class PhoneTest {
 
     public static void testPhone(Phone p) {
-        // Can only use the "call" method here, as it is the only one defined in the Phone interface
-        p.call("5105907277");
+        SmartPhone s = (SmartPhone) p;
+
+        s.call("5105907277");
+        s.playGame("Snake");
+        s.ringAlarm("An alarm");
+        s.printLastNumbers();
+        s.browseWeb("http://moodle.bbk.ac.uk");
+        s.findPosition();
     }
 
     public static void main(String[] args) {
         SmartPhone myPhone = new SmartPhone("Samsung");
 
         testPhone(myPhone);
-
-        // Won't work, as this method is not defined on MobilePhone
-        myPhone.browseWeb("http://moodle.bbk.ac.uk");
-
-        // Won't work, as this method is not defined on MobilePhone
-        myPhone.findPosition();
     }
 }
