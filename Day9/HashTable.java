76de265c08f6f3d7e8c4af8baa1fddf366b8d6fa
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-11-02 11:44:16 +0000

    Allow accessing single values in Day9 HashTable
    Re-write the HashTable to use a Stack to store the multiple values associated with each key, making it easy to access or remove just the most-recently added value.

--- Day9/HashTable.java
+++ Day9/HashTable.java
@@ -4,35 +4,58 @@ import java.util.Iterator;
  * A hash table that can store multiple values for each key.
  */
 public class HashTable<K, V> {
-    private SimpleMap<K, DoublyLinkedList<V>> storage;
+    private SimpleMap<K, GenericStack<V>> storage;
 
     public HashTable() {
-        storage = new SimpleMap<K, DoublyLinkedList<V>>();
+        storage = new SimpleMap<K, GenericStack<V>>();
     }
 
     /**
      * Adds the value to the list of values associates with the given key.
      */
     public void put(K key, V value) {
-        DoublyLinkedList<V> l = storage.get(key);
+        GenericStack<V> l = storage.get(key);
         if (l == null) {
-            l = new DoublyLinkedList<V>();
+            l = new GenericStack<V>();
             storage.put(key, l);
         }
-        l.add(value);
+        l.push(value);
     }
 
     /**
-     * Get an iterator over the values associated with the given key
+     * Get an iterator over the values associated with the given key, or null
+     * if there is no associated value.
      */
-    public Iterator<V> get(K key) {
-        return storage.get(key);
+    public Iterator<V> getAll(K key) {
+        GenericStack<V> s = storage.get(key);
+        if (s == null) {
+            return null;
+        } 
+        return s.iterator();
     } 
+
+    /**
+     * Gets the most recently added value for the given key.
+     */
+    public V getOne(K key) {
+        GenericStack<V> s = storage.get(key);
+        if (s == null) {
+            return null;
+        }
+        return s.peek();
+    }
+
+    /**
+     * Remove the most recent value associated with the given key.
+     */
+    public void deleteOne(K key) {
+        storage.get(key).pop(); 
+    }
 
     /**
      * Remove all the values associated with the given key.
      */
-    public void delete(K key) {
+    public void deleteAll(K key) {
         storage.delete(key);
     }
 }
