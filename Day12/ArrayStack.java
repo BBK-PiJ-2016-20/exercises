7d41d4c7750b6c955ba9f5d79d82ff7f75f7e6e7
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-11-09 15:44:20 +0000

    Add another stack implementation for Day 12 exercise 4.1
    Add an implementation based on arrays, and a test class.

---
+++ Day12/ArrayStack.java
@@ -0,0 +1,55 @@
+public class ArrayStack<T> implements Stack<T> {
+    private Object[] storage;
+    private int size;
+
+    private static final int INITIAL_SIZE = 1;
+
+    public ArrayStack() {
+        storage = new Object[INITIAL_SIZE];
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        return (T) storage[size - 1];
+    }
+
+    private void resize(int newSize) {
+        Object[] newStorage = new Object[newSize];
+        for (int i = 0; i < size; i++) {
+            newStorage[i] = storage[i];
+        }
+        storage = newStorage;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        if (size < storage.length / 4) {
+            resize(storage.length / 4);
+        }
+
+        T ret = (T) storage[size - 1];
+        storage[size - 1] = null;
+        size--;
+        return ret;
+    }
+
+    public void push(T t) {
+        if (size == storage.length) {
+            resize(storage.length * 4);
+        }
+        storage[size] = t;
+        size++;
+    }
+
+}
---
+++ Day12/ArrayStackTest.java
@@ -0,0 +1,5 @@
+public class ArrayStackTest extends StackTest {
+    protected Stack<Object> createStack() {
+        return new ArrayStack<Object>();
+    }
+}
