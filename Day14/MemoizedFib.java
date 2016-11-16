8f89ac13aef5358439dab9c9f9329cbbf417046f
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-11-16 14:34:35 +0000

    Altered Day 14 exercise 2 to use BigInteger
    This is now only limited by stack depth of the recursion. On the lab machine, this can get to about F(7300) before stack overflow (it seems to vary slightly from run to run, presumably depending on what else is using memory).

--- Day14/MemoizedFib.java
+++ Day14/MemoizedFib.java
@@ -1,16 +1,16 @@
+import java.math.BigInteger;
+
 public class MemoizedFib {
     /**
-     * Return the nth Fibonacci number. Can calculate up to F(92) before the
-     * long overflows (a previous version returning int could calculate up to
-     * F(46) before int overflow).
+     * Return the nth Fibonacci number.
      */
-    public static long fib(int n) {        
+    public static BigInteger fib(int n) {        
         // Create a memo array to store up to the nth Fibonacci number
-        long[] memo = new long[n];
-        memo[0] = 1; // 1st Fibonacci number is 1;
-        memo[1] = 1; // as is the second
+        BigInteger[] memo = new BigInteger[n];
+        memo[0] = new BigInteger("1"); // 1st Fibonacci number is 1;
+        memo[1] = new BigInteger("1"); // as is the second
         for (int i = 2; i < n; i++) {
-            memo[i] = -1;
+            memo[i] = new BigInteger("-1");
         }
         
         return fib(n, memo);
@@ -23,11 +23,11 @@ public class MemoizedFib {
      * @param   n       which Fibonacci number to calculate.
      * @param   memo    an array used for memoization. Must have a length of at least n.
      */
-    private static long fib(int n, long[] memo) {
-        if (memo[n-1] != - 1) {
+    private static BigInteger fib(int n, BigInteger[] memo) {
+        if (!memo[n-1].equals(new BigInteger("-1"))) {
             return memo[n-1];
         }
-        long nth = fib(n - 1, memo) + fib(n - 2, memo);
+        BigInteger nth = fib(n - 1, memo).add(fib(n - 2, memo));
         memo[n - 1] = nth;
         return nth;
     } 
@@ -40,7 +40,6 @@ public class MemoizedFib {
             n = Integer.parseInt(args[0]);
         }
 
-        System.out.println("        " + Long.MAX_VALUE);
         System.out.println("F(" + n + ") = " + fib(n));
     }
 }
