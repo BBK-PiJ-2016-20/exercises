64f8cec953abb3ef67fcda478e384d6a5ed1281b
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-11-16 14:19:41 +0000

    Add revised solution for Day 14 exercise 2
    Update fib to return a long, allowing for calculating up to F(92) before overflow.

--- Day14/MemoizedFib.java
+++ Day14/MemoizedFib.java
@@ -1,14 +1,18 @@
 public class MemoizedFib {
     /**
-     * Return the nth Fibonacci number
+     * Return the nth Fibonacci number. Can calculate up to F(92) before the
+     * long overflows (a previous version returning int could calculate up to
+     * F(46) before int overflow).
      */
-    public static int fib(int n) {
-        int[] memo = new int[n];
+    public static long fib(int n) {        
+        // Create a memo array to store up to the nth Fibonacci number
+        long[] memo = new long[n];
         memo[0] = 1; // 1st Fibonacci number is 1;
         memo[1] = 1; // as is the second
         for (int i = 2; i < n; i++) {
             memo[i] = -1;
         }
+        
         return fib(n, memo);
     } 
 
@@ -19,11 +23,11 @@ public class MemoizedFib {
      * @param   n       which Fibonacci number to calculate.
      * @param   memo    an array used for memoization. Must have a length of at least n.
      */
-    private static int fib(int n, int[] memo) {
+    private static long fib(int n, long[] memo) {
         if (memo[n-1] != - 1) {
             return memo[n-1];
         }
-        int nth = fib(n - 1, memo) + fib(n - 2, memo);
+        long nth = fib(n - 1, memo) + fib(n - 2, memo);
         memo[n - 1] = nth;
         return nth;
     } 
@@ -36,6 +40,7 @@ public class MemoizedFib {
             n = Integer.parseInt(args[0]);
         }
 
+        System.out.println("        " + Long.MAX_VALUE);
         System.out.println("F(" + n + ") = " + fib(n));
     }
 }
