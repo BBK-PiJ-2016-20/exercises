255dd0af013f57dba5b5e71bded6680399a23006
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-11-21 14:17:29 +0000

    Add modifications for Day 15 exercise 3b

--- Day15/UserInput.java
+++ Day15/UserInput.java
@@ -3,17 +3,21 @@
  * average. If the user inputs something that is not a number, the program
  * should complain and ask for a number again until 10 numbers have been
  * introduced.
+ *
+ * Modify the program so that it first asks how many numbers the user wants to
+ * enter, and then asks for the numbers. The computer should complain if the
+ * user enters something that is not a number in both cases. Use methods to
+ * prevent code repetitions.
  */
 public class UserInput {
-    public static void main(String[] args) {
-        double average = 0;
-        int entered = 0;
 
-        System.out.println("Enter 10 numbers");
-        while (entered < 10) {
-            System.out.print("A number: ");
+    public static double readNumber(String prompt) {
+        double number = 0;
+        boolean isNumber = false;
+
+        while (!isNumber) {
+            System.out.print(prompt);
             String input = System.console().readLine();
-            double number;
 
             try {
                 number = Double.parseDouble(input);
@@ -21,7 +25,21 @@ public class UserInput {
                 System.out.println(input + " is not a number, please try again");
                 continue;
             }
+            
+            isNumber = true;
+        }
 
+        return number;
+    }
+
+    public static void main(String[] args) {
+        double average = 0;
+        int entered = 0;
+
+        int toEnter = (int) readNumber("How many numbers? ");
+
+        while (entered < toEnter) {
+            double number = readNumber("Number " + (entered + 1) + " of " + toEnter + ": ");
             entered++;
             average = ((average * (entered - 1)) + number) / entered;
         }
