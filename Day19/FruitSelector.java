c9f9374051f5a8e22d9cf2c5c1036bfeec2ef6f1
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-12-05 15:28:15 +0000

    Add Day 19 live coding 3

--- Day19/FruitSelector.java
+++ Day19/FruitSelector.java
@@ -30,15 +30,26 @@ public class FruitSelector {
         return result;
     }
 
+    public static List<Apple> getApplesUnderWeight(List<Apple> apples, int limit) {
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : apples) {
+            if (apple.getWeight() <= limit) {
+                result.add(apple);
+            }
+        }
+
+        return result;
+    }
 
     public static void main(String[] args) {
         List<Apple> apples = new ArrayList<>();
         apples.add(new Apple(10, Colour.RED));
         apples.add(new Apple(15, Colour.GREEN));
         apples.add(new Apple(10, Colour.YELLOW));
-        apples.add(new Apple(13, Colour.BROWN));
+        apples.add(new Apple(200, Colour.BROWN));
         apples.add(new Apple(18, Colour.GREEN));
-        apples.add(new Apple(12, Colour.RED));
+        apples.add(new Apple(150, Colour.RED));
         apples.add(new Apple(19, Colour.RED));
 
         List<Apple> greenApples = getApplesByColour(apples, Colour.GREEN);
@@ -50,6 +61,17 @@ public class FruitSelector {
 
         System.out.println("Number of red apples: " + redApples.size());
         System.out.println(redApples);
+
+        List<Apple> heavyApples = getApplesOverWeight(apples, 150);
+
+        List<Apple> lightApples = getApplesUnderWeight(apples, 150);
+
+        System.out.println("Number of heavy apples: " + heavyApples.size());
+        System.out.println(heavyApples);
+
+        System.out.println("Number of light apples: " + lightApples.size());
+        System.out.println(lightApples);
+
 
     }
 
