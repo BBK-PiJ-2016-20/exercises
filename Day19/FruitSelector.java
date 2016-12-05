d651049a3eb022428d417d82ab111a94524a3e41
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-12-05 16:16:58 +0000

    Add Day 19 live coding 4

--- Day19/FruitSelector.java
+++ Day19/FruitSelector.java
@@ -42,6 +42,18 @@ public class FruitSelector {
         return result;
     }
 
+    public static List<Apple> getApplesIf(List<Apple> apples, ApplePredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : apples) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         List<Apple> apples = new ArrayList<>();
         apples.add(new Apple(10, Colour.RED));
@@ -52,28 +64,66 @@ public class FruitSelector {
         apples.add(new Apple(150, Colour.RED));
         apples.add(new Apple(19, Colour.RED));
 
-        List<Apple> greenApples = getApplesByColour(apples, Colour.GREEN);
+        List<Apple> greenApples = getApplesIf(apples, new AppleColourPredicate(Colour.GREEN));
 
         System.out.println("Number of green apples: " + greenApples.size());
         System.out.println(greenApples);
 
-        List<Apple> redApples = getApplesByColour(apples, Colour.RED);
+        List<Apple> redApples = getApplesIf(apples, new AppleColourPredicate(Colour.RED));
 
         System.out.println("Number of red apples: " + redApples.size());
         System.out.println(redApples);
 
-        List<Apple> heavyApples = getApplesOverWeight(apples, 150);
-
-        List<Apple> lightApples = getApplesUnderWeight(apples, 150);
+        List<Apple> heavyApples = getApplesIf(apples, new AppleOverWeightPredicate(150));
 
         System.out.println("Number of heavy apples: " + heavyApples.size());
         System.out.println(heavyApples);
 
+        List<Apple> lightApples = getApplesIf(apples, new AppleUnderWeightPredicate(150));
+
         System.out.println("Number of light apples: " + lightApples.size());
         System.out.println(lightApples);
-
-
     }
 
 }
 
+interface ApplePredicate {
+    boolean test(Apple apple);
+}
+
+class AppleOverWeightPredicate implements ApplePredicate {
+    private int limit;
+
+    public AppleOverWeightPredicate(int limit) {
+        this.limit = limit;
+    }
+
+    public boolean test(Apple apple) {
+        return apple.getWeight() > limit;
+    }
+}
+
+class AppleUnderWeightPredicate implements ApplePredicate {
+    private int limit;
+
+    public AppleUnderWeightPredicate(int limit) {
+        this.limit = limit;
+    }
+
+    public boolean test(Apple apple) {
+        return apple.getWeight() <= limit;
+    }
+}
+
+
+class AppleColourPredicate implements ApplePredicate {
+    private Colour colour;
+
+    public AppleColourPredicate(Colour colour) {
+        this.colour = colour;
+    }
+
+    public boolean test(Apple apple) {
+        return apple.getColour().equals(colour);
+    }
+}
