72e6cb7b91b46ab39eb8642981c8330631872dd1
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-12-07 15:18:35 +0000

    Add Day 20 exercise 8b

--- Day20/MapReduce.java
+++ Day20/MapReduce.java
@@ -5,11 +5,15 @@ import java.util.List;
  * Produce a single String that is the result of concatenating the uppercase
  * versions of all of the Strings. E.g., the result should be "HIHELLO...". Use
  * a single reduce operation, without using map.
+ *
+ * Produce the same String as above, but this time via a map operation that
+ * turns the words into upper case, followed by a reduce operation that
+ * concatenates them.
  */
  public class MapReduce {
      public static void main(String[] args) {
         List<String> words = Arrays.asList("hi", "hello", "bonjour", "tag", "hola", "yes", "no", "si", "oui", "non", "ja", "nein");
-        String changed = words.stream().reduce("", (x, y) -> x + y.toUpperCase());
+        String changed = words.stream().map(String::toUpperCase).reduce("", (x, y) -> x + y);
 
         System.out.println(changed);
      }
