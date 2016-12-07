fc2cbf1016198c1907af59314c615e02d5c9506a
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-12-07 15:20:08 +0000

    Add Day 20 exercise 8c

--- Day20/MapReduce.java
+++ Day20/MapReduce.java
@@ -9,11 +9,16 @@ import java.util.List;
  * Produce the same String as above, but this time via a map operation that
  * turns the words into upper case, followed by a reduce operation that
  * concatenates them.
+ *
+ * Produce a String that is all the words concatenated together, but with commas
+ * in between. E.g., the result should be "hi,hello,...". Note that there is no
+ * comma at the beginning, before "hi", and also no comma at the end, after the
+ * last word.
  */
  public class MapReduce {
      public static void main(String[] args) {
         List<String> words = Arrays.asList("hi", "hello", "bonjour", "tag", "hola", "yes", "no", "si", "oui", "non", "ja", "nein");
-        String changed = words.stream().map(String::toUpperCase).reduce("", (x, y) -> x + y);
+        String changed = words.stream().reduce((x, y) -> x + "," + y).orElse("");
 
         System.out.println(changed);
      }
