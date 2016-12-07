a1526a5cb41a3e376764a8ee963d87924f6c0af2
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-12-07 15:58:00 +0000

    Add Day 20 exercise 9b

--- Day20/Numbers.java
+++ Day20/Numbers.java
@@ -3,20 +3,32 @@ import java.util.Random;
 import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
-/**
- * Write a static method that produces a List of a specified length of random
- * numbers. E.g.: List<Double> nums = randomNumberList(someSize);
- *
- */
- public class Numbers {
-     public static void main(String[] args) {
-         List<Double> nums = randomNumberList(10);
+public class Numbers {
+    public static void main(String[] args) {
+        List<Double> randoms = randomNumberList(10);
+        System.out.println("10 random numbers: " + randoms);
 
-         System.out.println("10 random numbers: " + nums);
-     }
+        List<Integer> ordered = orderedNumbersList(50, 5, 10);
+        System.out.println("10 numbers starting at 50, increasing by 5: " + ordered);
+    }
 
-     public static List<Double> randomNumberList(int count) {
+    /**
+     * Write a static method that produces a List of a specified length of random
+     * numbers. E.g.: List<Double> nums = randomNumberList(someSize);
+     *
+     */
+    public static List<Double> randomNumberList(int count) {
          Random r = new Random();
          return Stream.generate(r::nextDouble).limit(count).collect(toList());
+     }
+
+     /**
+      * Write a static method that produces a list of numbers that go in order
+      * by a step size. E.g.: List<Integer> nums = orderedNumberList(50, 5,
+      * someSize);
+      *
+      */
+     public static List<Integer> orderedNumbersList(int start, int step, int count) {
+         return Stream.iterate(start, x -> x + step).limit(count).collect(toList());
      }
  }
