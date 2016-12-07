1a4d4a94ccaceeff403754774a167c4e48d1d758
Tim Fisken <tfiske01@dcs.bbk.ac.uk>
2016-12-07 14:48:06 +0000

    Add Day 20 exercise 5b

--- Day20/Printer.java
+++ Day20/Printer.java
@@ -4,10 +4,14 @@ import java.util.List;
 /**
  * Loop through the words and print each on a separate line, with two spaces in
  * front of each word.
+ *
+ * Repeat the previous problem, but without the two spaces in front. This is
+ * trivial if you use the same approach as in (5a), so the point here is to use
+ * a method reference.
  */
  public class Printer {
      public static void main(String[] args) {
          List<String> words = Arrays.asList("hi", "hello", "bonjour", "tag", "hola");
-         words.stream().map(word -> "  " + word).forEach(System.out::println);
+         words.stream().forEach(System.out::println);
      }
  }
